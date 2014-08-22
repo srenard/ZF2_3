1501cbe76cf01ef22d28e757a7bfc7bc5eeef743
srenard <srenard@ruses.com>
2014-08-22 19:52:47 +0200

    Après exercices sur Zend\Db\ResultSet

--- module/Application/src/Application/Controller/DbController.php
+++ module/Application/src/Application/Controller/DbController.php
@@ -11,8 +11,36 @@ class DbController extends AbstractActionController {
     public function test1Action() {
         $adapt = $this->getServiceLocator()->get('Zend\Db\Adapter\Adapter');
         $res = $adapt->query('SELECT * FROM `articles`', $adapt::QUERY_MODE_EXECUTE);
+        
+        //$r = $res->toArray();  //avec var_dump($r) dans la vue
+        //$r = $res->count();    //avec echo $r dans la vue
+        //$r = $res->getFieldCount();   //avec echo $r dans la vue
+        
+        //$res -> current();
+        //$res -> next();
+        //$r = $res -> current(); //avec echo var_dump($r) dans la vue
+        
+        //$res -> current();
+        //$res -> next();
+        //$res -> next();
+        //$res -> rewind();
+        //$r = $res->key(); //erreur. Rewind ne peut être utilisé sur ce type de result set
+        //This result is a forward only result set, calling rewind() 
+        //after moving forward is not supported
+        
+        //$res -> current();
+        //$res -> next();
+        //$r = $res -> key();
+        
+        //$res -> current();
+        //$res -> next();
+        //$res -> next();
+        //$res -> next();
+        //$r = $res->valid();//avec echo var_dump($r) dans la vue
+        
         $res = $res->current();
-        return new ViewModel(array('res' => $res));
+        return new ViewModel(array('res'=>$res));
+        //return new ViewModel(array('r' => $r));
     }
 
 }
