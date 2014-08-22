fce383266de1815ce58371f68d766a60a985daa7
srenard <srenard@ruses.com>
2014-08-22 20:02:14 +0200

    Après exemple d'utilisation de Zend\Db\Sql\Sql;

--- module/Application/src/Application/Controller/DbController.php
+++ module/Application/src/Application/Controller/DbController.php
@@ -5,21 +5,20 @@ namespace Application\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 use Zend\Db\Adapter\Adapter;
+use Zend\Db\Sql\Sql;
 
 class DbController extends AbstractActionController {
 
     public function test1Action() {
         $adapt = $this->getServiceLocator()->get('Zend\Db\Adapter\Adapter');
         $res = $adapt->query('SELECT * FROM `articles`', $adapt::QUERY_MODE_EXECUTE);
-        
+
         //$r = $res->toArray();  //avec var_dump($r) dans la vue
         //$r = $res->count();    //avec echo $r dans la vue
         //$r = $res->getFieldCount();   //avec echo $r dans la vue
-        
         //$res -> current();
         //$res -> next();
         //$r = $res -> current(); //avec echo var_dump($r) dans la vue
-        
         //$res -> current();
         //$res -> next();
         //$res -> next();
@@ -27,20 +26,25 @@ class DbController extends AbstractActionController {
         //$r = $res->key(); //erreur. Rewind ne peut être utilisé sur ce type de result set
         //This result is a forward only result set, calling rewind() 
         //after moving forward is not supported
-        
         //$res -> current();
         //$res -> next();
         //$r = $res -> key();
-        
         //$res -> current();
         //$res -> next();
         //$res -> next();
         //$res -> next();
         //$r = $res->valid();//avec echo var_dump($r) dans la vue
-        
-        $res = $res->current();
-        return new ViewModel(array('res'=>$res));
+        //$res = $res->current();
+        //return new ViewModel(array('res'=>$res));
         //return new ViewModel(array('r' => $r));
+
+        $adapt = $this->getServiceLocator()->get('Zend\Db\Adapter\Adapter');
+        $sql = new Sql($adapt);
+        $select = $sql->select();
+        $select->from('articles')->where("articles_nom = 'taille-crayon'");
+        $chaineSql = $sql->getSqlStringForSqlObject($select);
+        $resultat = $adapt->query($chaineSql, $adapt::QUERY_MODE_EXECUTE);
+        return new ViewModel(array('resultat' => $resultat));
     }
 
 }
