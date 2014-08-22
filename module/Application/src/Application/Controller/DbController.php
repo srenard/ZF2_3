dda492a2a04eb08f93432742ff8b908c1bec645d
srenard <srenard@ruses.com>
2014-08-22 21:23:51 +0200

    Après test de Zend\Db\Metadata\Metadata

--- module/Application/src/Application/Controller/DbController.php
+++ module/Application/src/Application/Controller/DbController.php
@@ -38,13 +38,58 @@ class DbController extends AbstractActionController {
         //return new ViewModel(array('res'=>$res));
         //return new ViewModel(array('r' => $r));
 
+        /*
+          $adapt = $this->getServiceLocator()->get('Zend\Db\Adapter\Adapter');
+          $sql = new Sql($adapt);
+          $select = $sql->select();
+          $select->from('articles')->where("articles_nom = 'taille-crayon'");
+          $chaineSql = $sql->getSqlStringForSqlObject($select);
+          echo $chaineSql;
+          $resultat = $adapt->query($chaineSql, $adapt::QUERY_MODE_EXECUTE);
+          return new ViewModel(array('resultat' => $resultat));
+         */
+
         $adapt = $this->getServiceLocator()->get('Zend\Db\Adapter\Adapter');
-        $sql = new Sql($adapt);
-        $select = $sql->select();
-        $select->from('articles')->where("articles_nom = 'taille-crayon'");
-        $chaineSql = $sql->getSqlStringForSqlObject($select);
-        $resultat = $adapt->query($chaineSql, $adapt::QUERY_MODE_EXECUTE);
-        return new ViewModel(array('resultat' => $resultat));
+// Instanciation de Zend\Db\Metadata\Metadata
+        $metadata = new \Zend\Db\Metadata\Metadata($adapt);
+// Obtention des noms de tables
+        $tableNames = $metadata->getTableNames();
+// On boucle sur les tables
+        foreach ($tableNames as $tableName) {
+            echo '<h3>Table ' . $tableName . "</h3>";
+// Obtention de la structure des tables
+            $table = $metadata->getTable($tableName);
+            echo 'Avec les colonnes : ' . "<br />";
+// On boucle sur les champs
+            foreach ($table->getColumns() as $column) {
+                echo $column->getName()
+                . ' -> ' . $column->getDataType()
+                . "<br />";
+            }
+            echo "<br />";
+            echo 'Avec les contraintes : ' . "<br />";
+// Obtention des contraintes
+            foreach ($metadata->getConstraints($tableName) as $constraint) {
+                /** @var $constraint Zend\Db\Metadata\Object\ConstraintObject */
+                echo $constraint->getName()
+                . ' -> ' . $constraint->getType()
+                . "<br />";
+                if (!$constraint->hasColumns()) {
+                    continue;
+                }
+                echo 'Colonne : ' . implode(', ', $constraint->getColumns());
+                if ($constraint->isForeignKey()) {
+                    $fkCols = array();
+                    foreach ($constraint->getReferencedColumns() as $refColumn) {
+                        $fkCols[] = $constraint->getReferencedTableName() .
+                                '.' . $refColumn;
+                    }
+                    echo ' => ' . implode(', ', $fkCols);
+                }
+                echo "<br />";
+            }
+            echo "<br /><br />";
+        }
     }
 
 }
