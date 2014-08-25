5fdaf2e25c84fdfbf63a121b08f4ea4fd6679d9f
srenard <srenard@ruses.com>
2014-08-25 18:41:41 +0200

    Après application d'un layout local

--- module/Routestest/src/Routestest/Controller/VoyageController.php
+++ module/Routestest/src/Routestest/Controller/VoyageController.php
@@ -8,7 +8,7 @@ use Zend\Mvc\Controller\AbstractActionController;
 class VoyageController extends AbstractActionController {
 
     public function accueilAction() {
-        $this->layout()->setTemplate('layout/layout2.phtml');
+        $this->layout()->setTemplate('routestest/layoutsLocaux/local_1.phtml');
         return new ViewModel(array('Pascale' => '20'));
     }
 
