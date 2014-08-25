7ffe8c2d88dec25084236f38e80b321cf5d87f99
srenard <srenard@ruses.com>
2014-08-25 21:06:17 +0200

    Après test des différentes routes du module routestest

--- module/Routestest/src/Routestest/Controller/VoyageController.php
+++ module/Routestest/src/Routestest/Controller/VoyageController.php
@@ -6,10 +6,32 @@ use Zend\View\Model\ViewModel;
 use Zend\Mvc\Controller\AbstractActionController;
 
 class VoyageController extends AbstractActionController {
+    /*
+      public function accueilAction() {
+      $this->layout()->setTemplate('routestest/layoutsLocaux/local_1.phtml');
+      return new ViewModel(array('Pascale' => '20'));
+      }
+     */
 
     public function accueilAction() {
-        $this->layout()->setTemplate('routestest/layoutsLocaux/local_1.phtml');
-        return new ViewModel(array('Pascale' => '20'));
+        $this->layout()->setTemplate('Routestest/layoutsLocaux/local_1.phtml');
+        $pays = $this->params()->fromRoute('pays');
+        return new ViewModel(array('Pascale' => '20', 'pays' => $pays));
+    }
+
+    public function accueilblogAction() {
+        $this->layout()->setTemplate('Routestest/layoutsLocaux/local_1.phtml');
+        return new ViewModel();
+    }
+
+    public function articleAction() {
+        $this->layout()->setTemplate('Routestest/layoutsLocaux/local_1.phtml');
+        return new ViewModel();
+    }
+
+    public function auteurAction() {
+        $this->layout()->setTemplate('Routestest/layoutsLocaux/local_1.phtml');
+        return new ViewModel();
     }
 
 }
